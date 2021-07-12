4a80d5671496ba226bf4297fc522b32e0d966680
Lenora Esquenazi <lenaciousd@gmail.com>
2021-07-12 09:17:02 -0700

    Sum using an index

--- src/recursion.js
+++ src/recursion.js
@@ -23,7 +23,13 @@ var factorial = function(n) {
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
-var sum = function(array) {
+var sum = function(array, index) {
+  var index = index || 0
+  if (!Array.isArray(array) || array.length === index) {
+    return 0
+  } else {
+    return array[index] + sum(array, index + 1)
+  }
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
